e21c2cdaf97c439ef811a20c515196f11caa6cc6
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2022-10-01 20:00:40 +0300

    Добавлена страница калькулятора В процессе дорабтка контроллера этой страницы

--- Calculator.Web/Controllers/CalculatorController.cs
+++ Calculator.Web/Controllers/CalculatorController.cs
@@ -14,17 +14,60 @@ namespace Calculator.Web.Controllers
             _plusService = plusService;
         }
 
-        public async Task<IActionResult> CalculatorIndex()
+        public async Task<IActionResult> CalculatorIndex(string firstValue, string secondValue, string operation)
+        {
+            CalculatorModel calculator = new CalculatorModel()
+            {
+                FirstValue = firstValue,
+                SecondValue = secondValue
+            };
+
+            decimal a = 0;
+            decimal b = 0;
+            string result = "";
+
+            if (!decimal.TryParse(firstValue, out a) || !decimal.TryParse(secondValue, out b))
+            {
+                calculator.Result = "Введите данные";
+                return View(calculator);
+            }
+
+            switch (operation)
+            {
+                case "+":
+                    result = (a + b).ToString();//GetPlusResult(firstValue, secondValue);
+                    break;
+                case "-":
+                    result = (a - b).ToString();
+                    break;
+                case "*":
+                    result = (a * b).ToString();
+                    break;
+                case "/":
+                    result = b != 0 ?
+                    (a / b).ToString()
+                    : "error";
+                    break;
+                default:
+                    result = "Введите данные";
+                    break;
+            }
+
+            calculator.Result = result;
+            return View(calculator);
+        }
+
+        private async Task<PlusResultDto> GetPlusResult(string firstValue, string secondValue)
         {
             PlusResultDto result = new();
-            var response = await _plusService.GetPlusResultAsync<ResponseDto>("1", "4");
+            var response = await _plusService.GetPlusResultAsync<ResponseDto>(firstValue, secondValue);
 
             if(response != null && response.IsSuccess)
             {
                 result = JsonConvert.DeserializeObject<PlusResultDto>(Convert.ToString(response.Result));
             }
 
-            return View(result);
+            return result;
         }
     }
 }
---
+++ Calculator.Web/Models/CalculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Web.Models
+{
+    public class CalculatorModel
+    {
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        public string FirstValue { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        public string SecondValue { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        public Operations Operation { get; set; }
+
+        public string Result { get; set; }
+
+        public enum Operations
+        {
+            Plus = '+',
+            Minus = '-',
+            Multiply = '*',
+            Divide = '/'
+        }
+    }
+}
